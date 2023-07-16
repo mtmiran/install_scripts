2cbd3268b7ba0f176cf3c56ceff69cbaa77339dd
mtmiran <mateuspmiranda@gmail.com>
2023-07-16 17:07:15 -0300

    update

--- debian/05_virtualization.sh
+++ debian/05_virtualization.sh
@@ -5,18 +5,18 @@
 # $ sudo systemctl restart systemd-networkd
 # https://linuxhint.com/install_configure_kvm_archlinux/
 
-
 virt=(qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils libguestfs-tools genisoimage virtinst libosinfo-bin)
 
 # execute
 for program in ${virt[@]}; do
-    sudo apt install -y "$program"
+	sudo apt install -y "$program"
 done
 
 # enable
 sudo adduser vivek libvirt
 sudo adduser vivek libvirt-qemu
 sudo systemctl start libvirtd.service
+sudo systemctl enable libvirtd.service
 sudo usermod -a -G libvirt $(whoami)
 newgrp libvirt
 sudo systemctl restart libvirtd.service
